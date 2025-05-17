bde4a35946fe539301c48ed380902558b6708722
jptbrandao@gmail.com <brandao@Mac.attlocal.net>
2025-05-17 10:13:29 -0500

    formating

--- nvim/lua/brandao/plugins/tree-sitter.lua
+++ nvim/lua/brandao/plugins/tree-sitter.lua
@@ -1,52 +1,52 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
-    build = ":TSUpdate",
-    dependencies = {
-        "windwp/nvim-ts-autotag",
-    },
-    config = function()
-        -- import nvim-treesitter plugin
-        local treesitter = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+	},
+	config = function()
+		-- import nvim-treesitter plugin
+		local treesitter = require("nvim-treesitter.configs")
 
-        -- configure treesitter
-        treesitter.setup({ -- enable syntax highlighting 
-            highlight = {
-                enable = true,
-            },
-            -- enable identation
-            indent = { enable = true },
-            -- enable autotagging (w/ nvim-ts-autotag plugin)
-            autotag = { 
-                enable = true,
-            },
-            -- ensure these language parsers are installed
-            ensure_installed = {
-                "json",
-                "javascript",
-                "typescript",
-                "tsx",
-                "yaml",
-                "html",
-                "css",
-                "markdown",
-                "markdown_inline",
-                "bash",
-                "lua",
-                "vim",
-                "dockerfile",
-                "gitignore",
-                "vimdoc",
-                "c",
-                "query",
-                "rust",
-                "bibtex",
-                "cpp",
-                "go",
-                "latex",
-                "python",
-                "sql",
-            },
-        })
-    end
+		-- configure treesitter
+		treesitter.setup({ -- enable syntax highlighting
+			highlight = {
+				enable = true,
+			},
+			-- enable identation
+			indent = { enable = true },
+			-- enable autotagging (w/ nvim-ts-autotag plugin)
+			autotag = {
+				enable = true,
+			},
+			-- ensure these language parsers are installed
+			ensure_installed = {
+				"json",
+				"javascript",
+				"typescript",
+				"tsx",
+				"yaml",
+				"html",
+				"css",
+				"markdown",
+				"markdown_inline",
+				"bash",
+				"lua",
+				"vim",
+				"dockerfile",
+				"gitignore",
+				"vimdoc",
+				"c",
+				"query",
+				"rust",
+				"bibtex",
+				"cpp",
+				"go",
+				"latex",
+				"python",
+				"sql",
+			},
+		})
+	end,
 }
