8b971ef144328d8801317c5bc8afc591db6204bf
jptbrandao@gmail.com <brandao@Mac.attlocal.net>
2025-06-02 11:18:12 -0500

    add keymaps to open files in new tabs or vertical splits

--- nvim/lua/brandao/plugins/nvim-tree.lua
+++ nvim/lua/brandao/plugins/nvim-tree.lua
@@ -1,41 +1,59 @@
 return {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-        local nvimtree = require("nvim-tree")
-        -- recommended settings from nvim-tree documentation
-        vim.g.loaded_netrw = 1
-        vim.g.loaded_netrwPlugin = 1
-        nvimtree.setup({
-            view = {
-                side = "right",
-                width = 40,
-            },   
-            renderer = {
-                indent_markers = {
-                    enable = true,
-                },
-            },
-            actions = {
-                open_file = {
-                    window_picker = {
-                        enable = false,
-                    },
-                },
-            },
-            filters = {
-                custom = { ".DS_Store" }
-            },
-            git = {
-                ignore = false
-            },
-        })
+	"nvim-tree/nvim-tree.lua",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		local nvimtree = require("nvim-tree")
+		-- recommended settings from nvim-tree documentation
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
 
-        local keymap = vim.keymap -- for conciseness
+		local function my_on_attach(bufnr)
+			local api = require("nvim-tree.api")
+			local function opts(desc)
+				return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+			end
 
-        keymap.set("n", "<C-n>", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer", noremap = true })
-        keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
-        keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" } ) 
-    end
+			local keymap = vim.keymap -- for conciseness
+			-- Open in new tab
+			keymap.set("n", "t", api.node.open.tab, opts("Open in New Tab"))
+
+			-- Open in vertical split
+			keymap.set("n", "v", api.node.open.vertical, opts("Open in Vertical Split"))
+
+			-- Default mappings
+			api.config.mappings.default_on_attach(bufnr)
+		end
+
+		nvimtree.setup({
+			on_attach = my_on_attach,
+			view = {
+				side = "right",
+				width = 40,
+			},
+			renderer = {
+				indent_markers = {
+					enable = true,
+				},
+			},
+			actions = {
+				open_file = {
+					window_picker = {
+						enable = false,
+					},
+				},
+			},
+			filters = {
+				custom = { ".DS_Store" },
+			},
+			git = {
+				ignore = false,
+			},
+		})
+
+		local keymap = vim.keymap -- for conciseness
+
+		keymap.set("n", "<C-n>", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer", noremap = true })
+		keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
+		keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
+	end,
 }
-
